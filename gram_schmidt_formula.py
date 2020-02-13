71ef17d3b3f49838ecfe24d675beed9c05a37376
vavu786 <55862742+vavu786@users.noreply.github.com>
2020-02-13 14:16:08 -0600

    Update gram_schmidt_formula.py
    Added capability of having a different number of vectors than the dimension of the vector space.
    Added helpful comments and introduction.

--- gram_schmidt_formula.py
+++ gram_schmidt_formula.py
@@ -1,20 +1,30 @@
+# ____________________________________________
+# Gram Schmidt Orthonormalization Calculator
+# The rows are the new basis vectors
+# Enjoy!
+# ____________________________________________
+
 import numpy as np
 import math as m
 from fractions import Fraction
-# np.set_printoptions(formatter={'all':lambda x: str(Fraction(x).limit_denominator())})
 
+# Defines specific inner product
 def inner(v1, v2):
     return np.dot(v1, v2)
 
+# Defines concept of "proj"
 def proj(v1, v2):
     return (inner(v1, v2)/inner(v2, v2))*v2
     
+# Orthogonalizes the vectors    
 def orthog(vecnum, ibasis, fbasis):
+    # vecnum is the nth vector in the basis vectors
     if vecnum == 0:
         fbasis[0,:] = ibasis[0,:]
     if vecnum != 0:
         fbasis[vecnum,:] = ibasis[vecnum,:] - sum(proj(ibasis[vecnum,:], fbasis[x,:]) for x in range(vecnum))
     
+# Divides each vector by its magnitude   
 def orthon(dim, vecnum, basis):
     mag = m.sqrt(sum((basis[vecnum,x])**2 for x in range(dim)))
     basis[vecnum,:] = basis[vecnum,:]/mag
@@ -22,12 +32,15 @@ def orthon(dim, vecnum, basis):
     
 def main():
     dim = int(input("What is the dimension of the vector space? "))
+    numOfVecs = int(input("How many vectors are in the basis of the vector space? "))
     basisnums = input("Enter the basis vectors, coordiates and vectors separated by spaces. \nFor example, (1, 0, 0), (0, 1, 0), and (0, 0, 1) would go as 1 0 0 0 1 0 0 0 1:\n")
-    basisVectors = np.asarray(basisnums.split(" ")).astype(int).reshape(dim, dim)
-    basisVectorsF = np.empty([dim, dim], dtype='float')
-    for i in range(dim):
+    # Inital basis vectors
+    basisVectors = np.asarray(basisnums.split(" ")).astype(int).reshape(numOfVecs, dim)
+    # Final basis vector matrix (empty at first, but gets filled up)
+    basisVectorsF = np.empty([numOfVecs, dim], dtype='float')
+    for i in range(numOfVecs):
         orthog(i, basisVectors, basisVectorsF)
-    for i in range(dim):
+    for i in range(numOfVecs):
         orthon(dim, i, basisVectorsF)
     print(basisVectorsF)
         
